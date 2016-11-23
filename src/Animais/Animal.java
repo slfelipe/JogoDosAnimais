4069834255dc92c450d0bfbe019d5befd932d671
slfelipe1 <fellpsl.18@gmail.com>
2016-11-23 00:52:26 -0200

    Jogo dos Animais
    - Exercício para Avaliação do 2º Bimestre da disciplina de Estrutura de
    Dados 1.

---
+++ src/Animais/Animal.java
@@ -0,0 +1,6 @@
+package Animais;
+public class Animal {
+	public String nome;
+	public Animal sim;
+	public Animal nao;
+}
---
+++ src/Animais/jogoAnimais.java
@@ -0,0 +1,101 @@
+package Animais;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+
+
+
+
+public class jogoAnimais {
+
+static public int contador=1;
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Animais");
+		JLabel label = new JLabel("Pense em um animal");
+		JButton btOkay = new JButton("OK");
+		
+		Animal animal = new Animal();
+		animal.nome = "Vive na Água";
+		
+		Animal tubarao = new Animal();
+		tubarao.nome = " Tubarão";
+		Animal macaco = new Animal();
+		macaco.nome = " Macaco";
+		
+		frame.setVisible(true);
+		frame.setLayout(null);
+		frame.setSize(230, 100);
+		frame.setLocationRelativeTo(null);
+		frame.add(label);
+		frame.add(btOkay);
+		
+		label.setBounds(45, -10, 150, 50);
+				
+		btOkay.setBounds(65, 30, 80, 20);
+
+	
+		animal.sim = tubarao;
+		animal.nao = macaco;
+		
+		btOkay.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+
+				encontrarAnimal(animal);
+
+			}
+		});
+	}
+
+
+
+	public static void encontrarAnimal(Animal animal) {
+
+		int optionPane = JOptionPane.YES_NO_OPTION;
+				int opcao = JOptionPane.showConfirmDialog(null, "O animal que você pensou " + animal.nome + "?", "Confirmar",
+				optionPane);
+
+		if (opcao == 0) {
+			if (animal.sim == null) {
+				if(contador==1){
+					JOptionPane.showMessageDialog(null, "Acertei!");
+				}else{
+				JOptionPane.showMessageDialog(null, "Acertei de Novo!");
+				}
+				contador++;
+			} else {
+				encontrarAnimal(animal.sim);
+			}
+
+		} else {
+			if (animal.nao == null) {
+
+				String animalNovo = JOptionPane.showInputDialog("Qual o animal que você pensou?");
+				String detalhesAnimal = JOptionPane
+						.showInputDialog("Um(a) " + animalNovo + "  _______ mas um(a)" + animal.nome + " não.");
+				
+				Animal caracteristica = new Animal();
+				Animal novo = new Animal();
+				
+				caracteristica.nome =  detalhesAnimal;
+				novo.nome = " é um " + animalNovo;
+				
+				caracteristica.sim = novo;
+				animal.nao= caracteristica;
+				
+				
+				
+			} else {
+				encontrarAnimal(animal.nao);
+			}
+		}
+
+	}
+
+}
